41b48dad2f87cf364d7d8fb3a92eef27dd678dff
Corbin Crutchley <crutchcorn@gmail.com>
2021-09-16 07:35:01 -0700

    Add initial effectScope demo

--- cjs-backcompat.cjs
+++
@@ -1,7 +0,0 @@
-const {ssrUtils} = require('@vue/runtime-core');
-const express = require('express')
-
-module.exports = {
-    ssrUtils,
-    express
-}
--- index.js
+++ index.js
@@ -1,52 +1,12 @@
-import {
-    createComponentInstance,
-    invokeLifeCycle,
-    LifecycleHooks
-} from "./create-component.js";
-import {setCurrentInstance} from "@vue/runtime-core";
-import {ref} from "@vue/reactivity";
-import {onMounted, onUpdated, watchEffect} from "@vue/runtime-core";
-import backcompat from './cjs-backcompat.cjs';
-const {express} = backcompat;
-const app = express()
-const port = 3009
+import {effectScope, ref} from "@vue/reactivity";
+import {watchEffect} from "@vue/runtime-core";
 
-const component = createComponentInstance()
-setCurrentInstance(component)
+const scope = effectScope()
 
-app.get('/', (req, res) => {
-    invokeLifeCycle(LifecycleHooks.MOUNTED, component)
-})
-
-
-app.get('/update', (req, res) => {
-    invokeLifeCycle(LifecycleHooks.UPDATED, component)
-    res.send();
-})
-
-app.get('/quit', (req, res) => {
-    invokeLifeCycle(LifecycleHooks.UNMOUNTED, component);
-    component.scope.stop();
-    res.send();
-    setTimeout(() => process.exit(), 0);
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
-
-component.scope.run(() => {
+scope.run(() => {
     const val = ref(0);
 
     console.log("I am doing some stuff");
-
-    onMounted(() => {
-        console.log("I am mounted");
-    })
-
-    onUpdated(() => {
-        console.log("I am updated");
-    })
 
     watchEffect((onInvalidate) => {
         console.log("Value has changed", val.value)
@@ -58,3 +18,7 @@ component.scope.run(() => {
 
     val.value++;
 })
+
+setTimeout(() => {
+    scope.stop();
+}, 1000)
